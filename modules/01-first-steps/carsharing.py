36f796a8648e521f84e8479a344eaf5fa9c5a0fe
Manisha <manisha7kumari7777@gmail.com>
2026-09-14 22:18:41 +0530

    Enhance car filtering and add ID retrieval endpoint
    Added filtering by doors and size in the car API, along with an endpoint to get a car by its ID.

--- modules/01-first-steps/carsharing.py
+++ modules/01-first-steps/carsharing.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
 db=[
     {"id":1,"size":"s","fuel":"petrol","doors":4,"transmission":"manual"},
@@ -29,9 +29,34 @@ async def get_cars(size=None):
     else :
         return [car for car in db if car['size']==size]
 
-#
+#com: This endpoint allows filtering cars by size and number of doors. If no query parameters are provided, it returns all cars. 
+#and also we have typecasted as via HTTP we get all query parameters as strings, so we explicitly cast doors to int
+
+@app.get("/api/cars/filter")
+async def filter_cars(size=None,doors:int=None):
+    if size is None and doors is None:
+        return db
+    elif size is None:
+        return [car for car in db if car['doors']==doors]
+    elif doors is None:
+        return [car for car in db if car['size']==size]
+    else:
+        return [car for car in db if car['size']==size and car['doors']==doors]
+    
+#com:Path parameter to get car by its ID
+@app.get("/api/cars/{id}")
+async def get_car_by_id(id:int):
+    out=[car for car in db if car['id']==id]
+    if not out:
+         raise HTTPException(status_code=404, detail="Car not found")
+    else:
+        return out
 
 #com: This is the welcome endpoint that greets the user by name
 @app.get("/")
 async def welcome(name):
     return {"message": f" hi {name} Hello !!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
